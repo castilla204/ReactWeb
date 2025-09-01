a4fda37e12bed41b04467ae98e10d2fb05140f99
castilla204 <tu_email@ejemplo.com>
2025-09-01 14:38:29 +0200

    subiendoo

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-const DEV_SERVER = import.meta.env.DEV ? '' : /*'https://api.atrapo.io'*/'http://localhost:7124'; 
+const DEV_SERVER = import.meta.env.DEV ? '' : 'https://api.atrapo.io'/*'http://localhost:7124'*/; 
 const API_PATH = '/api';
 
 export const API_CONFIG = {
